fea46e07a01f9d1bbcb207f84ba29fb1a63af99d
“Akhilesh <akhilsharma3332@email.com>
2024-09-28 21:05:49 +0530

    /auth/sIgnup created in Backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,32 +1,90 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
+require('dotenv').config()
+
+const connect = async () => {
+  try{
+    mongoose.connect(process.env.MONGODB_URL);
+    console.log("[🦊] MongoDB Atlas connected");
+  }catch(err){
+    console.log(err);
+  }
+}
+
+connect();
 
 
 /**
  * @swagger
- * /users:
- *   get:
- *     summary: Get all users
- *     responses:
- *       200:
- *         description: List of all users
- */
-router.get('/', (req, res) => {
-  res.send('Get all users');
-});
-
-/**
- * @swagger
- * /auth:
+ * /auth/signup:
  *   post:
  *     summary: Create a new user
+ *     description: This endpoint allows you to create a new user account.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: John
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *               password:
+ *                 type: string
+ *                 example: securepassword123
  *     responses:
  *       200:
- *         description: User created
+ *         description: User created successfully.
+ *       400:
+ *         description: Bad request, invalid input data.
+ *       500:
+ *         description: Internal server error.
  */
-router.post('/', (req, res) => {
-  res.send('Create a new user');
+router.post('/signup', async (req, res) => {
+    
+    console.log(req.body);
+
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName:  req.body.lastName,
+      email:  req.body.email,
+      password: req.body.password,
+    });
+    
+    // Hash the password before saving the user
+    const saltRounds = 10; // Cost factor for hashing
+    newUser.password = await bcrypt.hash(newUser.password, saltRounds);
+    
+    try {
+      const savedUser = await newUser.save();
+      console.log('User created:', savedUser);
+      
+    } catch (error) {
+      console.error('Error creating user:', error);
+    
+      const formattedError = {
+        message: error.message
+      };
+      
+      res.status(400).send(formattedError); 
+      return;
+    }
+
+    res.status(200).send("User Created Successfully");
 });
+
+
 
 /**
  * @swagger
