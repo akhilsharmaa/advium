6ac31882a5da39e0a68b894b3464f6ecbe33b775
“Akhilesh <akhilsharma3332@email.com>
2024-10-08 17:48:07 +0530

    Modified /new blog route, now whole body in the blog

--- backend/routes/write.js
+++ backend/routes/write.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const BlogBodySchema = require('../models/blogBody');
+const BlogBodySchema = require('../models/body');
 const authenticateJWT = require('../middleware/jwt');
-
+const logger = require("../logger/logger")
 
 const router = express.Router();
 
@@ -23,9 +23,6 @@ const router = express.Router();
  *           schema:
  *             type: object
  *             properties:
- *               post_id:
- *                 type: string
- *                 example: "60d21b4667d0d8992e610c86"  # Post ID
  *               title:
  *                 type: string
  *                 example: "My First Blog Post"
@@ -40,6 +37,9 @@ const router = express.Router();
  *               thumbnailBase64:
  *                 type: string
  *                 example: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA..."
+ *               secondaryThumbnailBase64:
+ *                 type: string
+ *                 example: "data:image/png;base64,iVB123123sdfAANSUhEUgAAAAUA..."
  *     responses:
  *       200:
  *         description: Blog created successfully
@@ -68,8 +68,9 @@ const router = express.Router();
 
 router.post('/new', authenticateJWT, async (req, res) => {
 
+    // Create a new BlogBody object based on the request body data
     const newBlogBody = new BlogBodySchema({
-        author_id: req.body._id,
+        authorId: req.body._id,
         post_id: req.body.post_id,
         title: req.body.title,
         markdown_body: req.body.markdown_body,
@@ -79,9 +80,15 @@ router.post('/new', authenticateJWT, async (req, res) => {
 
     try {
         
-        newBlogBody.addNewStatus("Submitted");
-        await newBlogBody.save(); 
-        
+        // These methods initialize author ID, timestamp, and status initialization
+        newBlogBody.initializeAuthorId(req.body._id);
+        newBlogBody.initializeTime();
+        newBlogBody.initializeStatus();
+
+        // Save the new blog body to the database
+        await newBlogBody.save();         
+        logger.info(`🚀 New Blog Pushed!  BlogId:  ${newBlogBody._id}`)
+
         return res.status(200).json({ 
             "message": "Blog Created Successfully", 
             "blogBodyId": newBlogBody._id
